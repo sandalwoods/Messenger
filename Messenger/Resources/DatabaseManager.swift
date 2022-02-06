492bde423e24d2a535594cf3edf321e71ed899d9
sandalwoods <1764076874@qq.com>
2022-02-06 13:55:34 +0800

    Handle auth session, add database

---
+++ Messenger/Resources/DatabaseManager.swift
@@ -0,0 +1,42 @@
+//
+//  DatabaseManager.swift
+//  Messenger
+//
+//  Created by Kevin on 2022/2/6.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    static let shared = DatabaseManager()
+    private let database = Database.database().reference()
+}
+
+//MARK: - Account Management
+extension DatabaseManager {
+    
+    public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
+        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+            }
+            completion(true)
+        })
+    }
+    ///Insert new user to databse
+    public func insertUser(with user: ChatAppUser) {
+        database.child(user.emailAddress).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName
+        ])
+    }
+}
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+//    let profilePictureUrl: String
+}
